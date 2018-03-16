a704f3072b700a7ef3ebdfd731de3413cdd18925
phpetrov1885 <31486310+phpetrov1885@users.noreply.github.com>
2018-03-16 16:14:00 +0200

    DefineClassPerson

---
+++ Defining Classes/DefineClassPerson/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class Person
+{
+    private string name;
+    private int age;
+
+    
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+    public int Age
+    {
+        get { return age; }
+        set { age = value; }
+    }
+}
+
