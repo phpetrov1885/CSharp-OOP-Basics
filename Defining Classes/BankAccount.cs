7b769d9a5bd2fa9de2202221e34b134c26702f3a
phpetrov1885 <31486310+phpetrov1885@users.noreply.github.com>
2018-03-19 18:22:06 +0200

    BankAccount

---
+++ Defining Classes/BankAccount.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+
+public class BankAccount
+{
+    private int id;
+    public int Id { get; set; }
+
+    private decimal balance;
+    public decimal Balance { get; set; }
+
+
+    public void Deposit(decimal amount)
+    {
+        Balance += amount;
+    }
+    public void Withdraw(decimal amount)
+    {
+        if (Balance < amount)
+        {
+            Console.WriteLine("Insufficient balance");
+        }
+        else
+        {
+            Balance -= amount;
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"Account ID{Id}, balance {Balance:f2}";
+    }
+}
+
