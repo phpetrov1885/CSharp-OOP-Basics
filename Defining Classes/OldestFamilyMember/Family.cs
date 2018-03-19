7c35469041d0bdb7058d437d7ddefe2362486504
phpetrov1885 <31486310+phpetrov1885@users.noreply.github.com>
2018-03-19 18:25:06 +0200

    OldestFamilyMember

---
+++ Defining Classes/OldestFamilyMember/Family.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+class Family
+{
+    private List<Person> familyMembers;
+
+    public Family()
+    {
+        familyMembers=new List<Person>();
+    }
+    public List<Person> FamilyMembers
+    {
+        get { return familyMembers; }
+        set { familyMembers = value; }
+    }
+
+    public void AddMember(Person familyMember)
+    {
+        familyMembers.Add(familyMember);
+    }
+
+    public Person GetOdestMember()
+    {
+       return familyMembers.OrderByDescending(s => s.Age).FirstOrDefault();
+    }
+}
+
---
+++ Defining Classes/OldestFamilyMember/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Family family = new Family();
+        int numMembers = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < numMembers; i++)
+        {
+            string[] splitInput = Console.ReadLine().Split();
+            Person familyPerson = new Person(splitInput[0], int.Parse(splitInput[1]));
+
+            family.AddMember(familyPerson);
+
+        }
+        Person oldestFamilyMember = family.GetOdestMember();
+
+        Console.WriteLine($"{oldestFamilyMember.Name} {oldestFamilyMember.Age}");
+    }
+}
+
