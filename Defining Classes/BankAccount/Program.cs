66d4c780d5a8674816e8a41bb1b39918e1851214
phpetrov1885 <31486310+phpetrov1885@users.noreply.github.com>
2018-03-19 18:23:22 +0200

    BankAccount

---
+++ Defining Classes/BankAccount/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var accounts = new Dictionary<int, BankAccount>();
+        string commands = Console.ReadLine();
+
+        while (commands != "End")
+        {
+            var splitCommand = commands.Split();
+            var accountId = int.Parse(splitCommand[1]);
+
+            switch (splitCommand[0])
+            {
+                case "Create":
+                    if (!accounts.ContainsKey(accountId))
+                    {
+                        var account = new BankAccount();
+                        account.Id = accountId;
+                        accounts.Add(accountId, account);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Account already exists");
+                    }
+                    break;
+                case "Deposit":
+                    if (ValidatePersonAccount(accountId, accounts))
+                    {
+                        accounts[accountId].Deposit(int.Parse(splitCommand[2]));
+                    }
+                    break;
+                case "Withdraw":
+                    if (ValidatePersonAccount(accountId, accounts))
+                    {
+                        accounts[accountId].Withdraw(int.Parse(splitCommand[2]));
+                    }
+                    break;
+                case "Print":
+                    if (ValidatePersonAccount(accountId, accounts))
+                    {
+                        Console.WriteLine(accounts[accountId]);
+                    }
+                    break;
+            }
+
+            commands = Console.ReadLine();
+        }
+    }
+
+    static bool ValidatePersonAccount(int accountId, Dictionary<int, BankAccount> accounts)
+    {
+        if (accounts.ContainsKey(accountId))
+        {
+            return true;
+        }
+        else
+        {
+            Console.WriteLine("Account does not exist");
+            return false;
+        }
+       
+    }
+}
+
