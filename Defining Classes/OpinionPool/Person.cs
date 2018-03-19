a5b292f44e91d899b508251b5af6dfaf6fd8b1cc
phpetrov1885 <31486310+phpetrov1885@users.noreply.github.com>
2018-03-19 18:25:36 +0200

    OpinionPool

---
+++ Defining Classes/OpinionPool/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+class Person
+{
+    private string name;
+    private int age;
+
+    public Person(string name,int age)
+    {
+        Name = name;
+        Age = age;
+    }
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+
+    public int Age
+    {
+        get { return age; }
+        set { age = value; }
+    }
+
+    public override string ToString()
+    {
+        return $"{Name} - {Age}";
+    }
+}
+
---
+++ Defining Classes/OpinionPool/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Person> totalPersons = new List<Person>();
+        int numberOfPersons = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < numberOfPersons; i++)
+        {
+            string[] splitInput = Console.ReadLine().Split();
+            Person person = new Person(splitInput[0], int.Parse(splitInput[1]));
+
+            totalPersons.Add(person);
+        }
+       
+        foreach (var person in totalPersons.OrderBy(p=>p.Name))
+        {
+            if (person.Age>30)
+            {
+                Console.WriteLine(person);
+            }
+        }
+    }
+}
+
