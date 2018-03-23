623e458e38ff898bf24af170f8718ef29e952789
phpetrov1885 <31486310+phpetrov1885@users.noreply.github.com>
2018-03-23 18:29:49 +0200

    SpeedRacing

---
+++ Defining Classes/SpeedRacing/Car.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class Car
+{
+    private string carModel;
+    private decimal fuelAmount;
+    private decimal fuelConsumptionPerKM;
+    private decimal distanceTraveled;
+
+    public Car(string carModel, decimal fuelAmount, decimal fuelConsumptionPerKM)
+    {
+        CarModel = carModel;
+        FuelAmount = fuelAmount;
+        FuelConsumptionPerKM = fuelConsumptionPerKM;
+    }
+
+    public string CarModel
+    {
+        get { return carModel; }
+        set { carModel = value; }
+    }
+    public decimal FuelAmount
+    {
+        get { return fuelAmount; }
+        set { fuelAmount = value; }
+    }
+    public decimal FuelConsumptionPerKM
+    {
+        get { return fuelConsumptionPerKM; }
+        set { fuelConsumptionPerKM = value; }
+    }
+    public decimal DistanceTraveled
+    {
+        get { return distanceTraveled; }
+        set { distanceTraveled = value; }
+    }
+
+    public void GetDistanceTravelled(decimal amountOfKm)
+    {
+        if (amountOfKm * fuelConsumptionPerKM > fuelAmount)
+        {
+            Console.WriteLine($"Insufficient fuel for the drive");
+        }
+        else
+        {
+            fuelAmount -= amountOfKm * fuelConsumptionPerKM;
+            distanceTraveled += amountOfKm;
+        }
+    }
+}
+
---
+++ Defining Classes/SpeedRacing/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Car> cars = new List<Car>();
+        int numberCars = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < numberCars; i++)
+        {
+            ReadCarSpecsAndAddCar(cars);
+        }
+
+        string driveInput = Console.ReadLine();
+        while (driveInput != "End")
+        {
+            SplitCommandAndTakeCar(driveInput, cars);
+
+            driveInput = Console.ReadLine();
+        }
+        PrintFuelAmountAndDistance(cars);
+    }
+
+    private static void SplitCommandAndTakeCar(string driveInput, List<Car> cars)
+    {
+        string[] splitInput = driveInput.Split();
+        string carModel = splitInput[1];
+        decimal amountOfKm = decimal.Parse(splitInput[2]);
+
+        if (splitInput[0] == "Drive")
+        {
+            cars.Where(c => c.CarModel == carModel).First().GetDistanceTravelled(amountOfKm);
+        }
+    }
+
+    private static void ReadCarSpecsAndAddCar(List<Car> cars)
+    {
+        string[] splitInput = Console.ReadLine().Split();
+        string carModel = splitInput[0];
+        decimal amountOfFuel = decimal.Parse(splitInput[1]);
+        decimal fuelConsumption = decimal.Parse(splitInput[2]);
+
+        var car = new Car(carModel, amountOfFuel, fuelConsumption);
+        cars.Add(car);
+    }
+
+
+    private static void PrintFuelAmountAndDistance(List<Car> cars)
+    {
+        foreach (var car in cars)
+        {
+            Console.WriteLine($"{car.CarModel} {car.FuelAmount:f2} {car.DistanceTraveled}");
+        }
+    }
+}
+
