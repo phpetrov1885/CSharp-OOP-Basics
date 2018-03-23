a362a84de9d9fe041d8b7eb3c9acb07828e1bd3d
phpetrov1885 <31486310+phpetrov1885@users.noreply.github.com>
2018-03-23 18:24:54 +0200

    PokemonTrainer

---
+++ Defining Classes/PokemonTrainer/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+class Program
+{
+    static void Main()
+    {
+        List<Pokemon> pokemons = new List<Pokemon>();
+        List<Trainer> trainers = new List<Trainer>();
+        
+        string input = Console.ReadLine();
+        AddPokemonAndTrainerInformationToCollections(input, pokemons, trainers);
+
+        string element = Console.ReadLine();
+        CheckForPokemonElementsAndPlay(element, pokemons, trainers);
+
+        PrintTrainers(trainers);
+        
+    }
+
+    private static void PrintTrainers(List<Trainer> trainers)
+    {
+        foreach (var trainer in trainers.OrderByDescending(t => t.Badges))
+        {
+            Console.WriteLine($"{trainer.Name} {trainer.Badges} {trainer.Pokemons.Count}");
+        }
+    }
+
+    private static void CheckForPokemonElementsAndPlay(string element, List<Pokemon> pokemons, List<Trainer> trainers)
+    {
+        while (element != "End")
+        {
+            foreach (var trainer in trainers)
+            {
+                var IsElementExist = trainer.Pokemons.Exists(p => p.Element == element);
+                if (IsElementExist)
+                {
+                    trainer.AddBadge();
+                }
+                else
+                {
+                    foreach (var pokemon in trainer.Pokemons)
+                    {
+                        pokemon.LoseHealth();
+                        if (pokemon.Health <= 0)
+                        {
+                            trainer.RemovePokemon(pokemon);
+                            break;
+                        }
+                    }
+                }
+            }
+
+            element = Console.ReadLine();
+        }
+    }
+
+    private static void AddPokemonAndTrainerInformationToCollections(string input, List<Pokemon> pokemons, List<Trainer> trainers)
+    {
+        while (input != "Tournament")
+        {
+            string[] pokemonArgs = input.Split();
+            string trainerName = pokemonArgs[0];
+            string pokemonName = pokemonArgs[1];
+            string pokemonElement = pokemonArgs[2];
+            int pokemonHealth = int.Parse(pokemonArgs[3]);
+
+            Pokemon pokemon = new Pokemon(pokemonName, pokemonElement, pokemonHealth);
+            pokemons.Add(pokemon);
+
+            if (!trainers.Any(t => t.Name == trainerName))
+            {
+                Trainer trainer = new Trainer(trainerName, 0);
+                trainer.Pokemons.Add(pokemon);
+                trainers.Add(trainer);
+            }
+            else
+            {
+                var Trainer = trainers.Where(t => t.Name == trainerName).First();
+                Trainer.Pokemons.Add(pokemon);
+
+            }
+
+            input = Console.ReadLine();
+        }
+    }
+}
+
---
+++ Defining Classes/PokemonTrainer/Trainer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+class Trainer
+{
+    private string name;
+    private int badges;
+    private List<Pokemon> pokemons;
+
+    public Trainer()
+    {
+        this.pokemons = new List<Pokemon>();
+    }
+    public Trainer(string name,int badges) : this()
+    {
+        this.Name = name;
+        this.Badges = badges;
+    }
+
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+    public int Badges
+    {
+        get { return badges; }
+        set { badges = value; }
+    }
+    public List<Pokemon> Pokemons
+    {
+        get { return pokemons; }
+        set { pokemons = value; }
+    }
+
+    public void AddBadge()
+    {
+        Badges++;
+    }
+
+    public void RemovePokemon(Pokemon pokemon)
+    {
+        Pokemons.Remove(pokemon);
+    }
+}
+
