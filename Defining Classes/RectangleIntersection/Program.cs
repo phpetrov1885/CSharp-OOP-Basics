8561966638810cb0f7275bc15a602009dfa68d84
phpetrov1885 <31486310+phpetrov1885@users.noreply.github.com>
2018-03-23 18:28:31 +0200

    RectangleIntersection

---
+++ Defining Classes/RectangleIntersection/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Rectangle> rectangles = new List<Rectangle>();
+        int[] inputNumbs = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int rectanglesTotal = inputNumbs[0];
+        int checks = inputNumbs[1];
+
+        SplitAndAddRectangle(rectangles, rectanglesTotal);
+
+        ReadRectanglesIdAndCheckForIntersection(checks, rectangles);
+    }
+
+    private static void ReadRectanglesIdAndCheckForIntersection(int checks, List<Rectangle> rectangles)
+    {
+        for (int i = 0; i < checks; i++)
+        {
+            string[] rectanglesNames = Console.ReadLine().Split();
+            string firstRectangleName = rectanglesNames[0];
+            string secondRectangleName = rectanglesNames[1];
+
+            if (rectangles.Any(r => r.Id == firstRectangleName)
+                && rectangles.Any(r => r.Id == secondRectangleName))
+            {
+                Rectangle firstRectangle = rectangles.Where(r => r.Id == firstRectangleName).First();
+                Rectangle secondRectangle = rectangles.Where(r => r.Id == secondRectangleName).First();
+                if (firstRectangle.IsIntersect(secondRectangle))
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine("false");
+                }
+            }
+        }
+    }
+
+    private static void SplitAndAddRectangle(List<Rectangle> rectangles, int rectanglesTotal)
+    {
+        for (int i = 0; i < rectanglesTotal; i++)
+        {
+            string[] rectangleArgs = Console.ReadLine().Split();
+
+            string rectangleName = rectangleArgs[0];
+            int rectangleWidht = int.Parse(rectangleArgs[1]);
+            int rectangleHeight = int.Parse(rectangleArgs[2]);
+            int topLeftX = int.Parse(rectangleArgs[3]);
+            int topLeftY = int.Parse(rectangleArgs[4]);
+
+            Rectangle rectangle = new Rectangle(rectangleName, rectangleWidht, rectangleHeight,
+                topLeftX, topLeftY);
+
+            rectangles.Add(rectangle);
+        }
+    }
+}
+
---
+++ Defining Classes/RectangleIntersection/Rectangle.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+class Rectangle
+{
+    private string id;
+    private int width;
+    private int height;
+    private int topLeftX;
+    private int topLeftY;
+
+    public Rectangle()
+    {
+    }
+    public Rectangle(string id, int width, int height, int topLeftX, int topLeftY)
+    {
+        Id = id;
+        Width = width;
+        Height = height;
+        TopLeftX = topLeftX;
+        TopLeftY = topLeftY;
+    }
+
+    public string Id
+    {
+        get { return id; }
+        set { id = value; }
+    }
+    public int Width
+    {
+        get { return width; }
+        set { width = value; }
+    }
+    public int Height
+    {
+        get { return height; }
+        set { height = value; }
+    }
+    public int TopLeftX
+    {
+        get { return topLeftX; }
+        set { topLeftX = value; }
+    }
+    public int TopLeftY
+    {
+        get { return topLeftY; }
+        set { topLeftY = value; }
+    }
+
+    public bool IsIntersect(Rectangle rectangle)
+    {
+        return (this.topLeftX >= rectangle.topLeftX &&
+                this.topLeftX <= rectangle.topLeftX + width &&
+                this.topLeftY + height >= rectangle.topLeftY &&
+                this.topLeftY + height <= rectangle.topLeftY + height)
+               ||
+               (this.topLeftX + width >= rectangle.topLeftX &&
+                this.topLeftX + width <= rectangle.topLeftX + width &&
+                this.topLeftY + width >= rectangle.topLeftY &&
+                this.topLeftY + width <= rectangle.topLeftY + height)
+               ||
+               (this.topLeftX >= rectangle.topLeftX &&
+                this.topLeftX <= rectangle.topLeftX + width &&
+                this.topLeftY >= rectangle.topLeftY &&
+                this.topLeftY <= rectangle.topLeftY + height)
+               ||
+               (this.topLeftX + width >= rectangle.topLeftX &&
+                this.topLeftX + width <= rectangle.topLeftX + width &&
+                this.topLeftY + height >= rectangle.topLeftY &&
+                this.topLeftY + height <= rectangle.topLeftY + height);
+    }
+}
+
