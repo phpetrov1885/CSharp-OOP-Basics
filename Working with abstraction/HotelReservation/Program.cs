a5faa63cf7496b806de8093f49e03ba1c6729720
phpetrov1885 <31486310+phpetrov1885@users.noreply.github.com>
2018-03-27 17:06:54 +0300

    HotelReservation

---
+++ Working with abstraction/HotelReservation/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HotelReservation
+{
+    class Program
+    {
+        static void Main()
+        {
+            var command = Console.ReadLine();
+
+            var priceCalculator = new PriceCalculator(command);
+            var totalPrice = priceCalculator.CalculatePrice();
+
+            Console.WriteLine(totalPrice);
+        }
+    }
+}
