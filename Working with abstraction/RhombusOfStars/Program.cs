1c7bd74df873d7606c2118e40334fc730ca48185
phpetrov1885 <31486310+phpetrov1885@users.noreply.github.com>
2018-03-27 17:08:39 +0300

    RhombusOfStars

---
+++ Working with abstraction/RhombusOfStars/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace RhombusOfStars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rhombusSize = int.Parse(Console.ReadLine());
+
+            for (int rowSize = 1; rowSize < rhombusSize; rowSize++)
+            {
+                PrintRow(rhombusSize,rowSize);
+            }
+            for (int rowSize = rhombusSize-1; rowSize > 0; rowSize--)
+            {
+                PrintRow(rhombusSize, rowSize);
+            }
+
+        }
+
+        private static void PrintRow(int rhombusSize, int rowSize)
+        {
+            for (int j = 1; j < rhombusSize-rowSize; j++)
+            {
+                Console.Write(" ");
+            }
+
+            for (int i = 0; i < rowSize; i++)
+            {
+                Console.Write("* ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
