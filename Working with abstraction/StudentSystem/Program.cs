0607b00a909835d1d2cc6cac45dec430275fb999
phpetrov1885 <31486310+phpetrov1885@users.noreply.github.com>
2018-03-27 17:09:12 +0300

    StudentSystem

---
+++ Working with abstraction/StudentSystem/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var studentSystem = new StudentSystem();
+        string command = Console.ReadLine();
+
+        while (command != "End")
+        {
+            studentSystem.ParseCommand(command, Console.WriteLine);
+
+            command = Console.ReadLine();
+        }
+    }
+}
+
---
+++ Working with abstraction/StudentSystem/StudentSystem.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+
+public class StudentSystem
+{
+    private Dictionary<string, Student> repo;
+
+    public StudentSystem()
+    {
+        this.repo = new Dictionary<string, Student>();
+    }
+
+
+    public void ParseCommand(string command, Action<string> printFunc)
+    {
+        string[] args = command.Split();
+
+        if (args[0] == "Create")
+        {
+            CreateStudent(args);
+        }
+        else if (args[0] == "Show")
+        {
+            var studentName = args[1];
+            if (repo.ContainsKey(studentName))
+            {
+                printFunc(repo[studentName].ToString());
+            }
+        }
+
+    }
+
+
+    private void CreateStudent(string[] args)
+    {
+        var name = args[1];
+        var age = int.Parse(args[2]);
+        var grade = double.Parse(args[3]);
+        if (!repo.ContainsKey(name))
+        {
+            var student = new Student(name, age, grade);
+            repo[name] = student;
+        }
+    }
+}
+
+
