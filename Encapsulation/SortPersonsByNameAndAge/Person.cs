b00c6539678bbb5da4b7691a19efca89203d3149
phpetrov1885 <31486310+phpetrov1885@users.noreply.github.com>
2018-04-02 17:20:18 +0300

    SortPersonsByNameAndAge

---
+++ Encapsulation/SortPersonsByNameAndAge/Person.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class Person
+{
+    private string firstName;
+    private string lastName;
+    private int age;
+
+    public Person()
+    {
+    }
+    public Person(string firstName, string lastName, int age)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Age = age;
+    }
+
+    public string FirstName
+    {
+        get { return firstName; }
+        set { firstName = value; }
+    }
+    public string LastName
+    {
+        get { return lastName; }
+        set { lastName = value; }
+    }
+    public int Age
+    {
+        get { return age; }
+        set { age = value; }
+    }
+
+    public override string ToString()
+    {
+        return $"{firstName} {lastName} is {age} years old.";
+    }
+}
+
---
+++ Encapsulation/SortPersonsByNameAndAge/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+
+
+class Program
+{
+    static void Main()
+    {
+        List<Person>persons=new List<Person>();
+        int lines = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < lines; i++)
+        {
+            SplitPersonArgsAndAddtoCollection(persons);
+        }
+
+        PrintPersonsByOrder(persons);
+    }
+
+    private static void PrintPersonsByOrder(List<Person> persons)
+    {
+        foreach (var person in persons.OrderBy(p=>p.FirstName).ThenBy(p=>p.Age))
+        {
+            Console.WriteLine(person);
+        }
+    }
+
+    private static void SplitPersonArgsAndAddtoCollection(List<Person> persons)
+    {
+        string[] personArgs = Console.ReadLine().Split();
+        string firstName = personArgs[0];
+        string lastName = personArgs[1];
+        int age = int.Parse(personArgs[2]);
+        Person currentPerson = new Person(firstName, lastName, age);
+        persons.Add(currentPerson);
+    }
+}
+
