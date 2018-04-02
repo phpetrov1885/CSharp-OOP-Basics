85d155eccab89c0e850e8038a0c01e07d99b9277
phpetrov1885 <31486310+phpetrov1885@users.noreply.github.com>
2018-04-02 17:21:39 +0300

    ValidationОfData

---
+++ Encapsulation/ValidationОfData/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var lines = int.Parse(Console.ReadLine());
+        var persons = new List<Person>();
+        for (int i = 0; i < lines; i++)
+        {
+            SplitPersonArgsAndAddtoCollection(persons);
+        }
+        var bonus = decimal.Parse(Console.ReadLine());
+
+        PrintPersons(persons, bonus);
+
+    }
+    private static void PrintPersons(List<Person> persons, decimal bonus)
+    {
+        foreach (var person in persons)
+        {
+            person.IncreaseSalary(bonus);
+            Console.WriteLine(person);
+        }
+    }
+    private static void SplitPersonArgsAndAddtoCollection(List<Person> persons)
+    {
+        string[] personArgs = Console.ReadLine().Split();
+        string firstName = personArgs[0];
+        string lastName = personArgs[1];
+        int age = int.Parse(personArgs[2]);
+        decimal salary = decimal.Parse(personArgs[3]);
+
+        Person currentPerson = new Person(firstName, lastName, age, salary);
+        persons.Add(currentPerson);
+    }
+}
+
