32b301f5fbb6565e1a96406314315a871595ef60
phpetrov1885 <31486310+phpetrov1885@users.noreply.github.com>
2018-04-19 21:44:39 +0300

    StackOfStrings

---
+++ Inheritance/StackOfStrings/StackOFString.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class StackOFString
+{
+    List<string>data=new List<string>();
+
+    public bool IsEmpty()
+    {
+        return data.Count == 0;
+    }
+
+    public void Push(string element)
+    {
+        data.Add(element);
+    }
+
+    public string Peek()
+    {
+        string result = string.Empty;
+        if (!IsEmpty())
+        {
+            result = data[data.Count - 1];
+        }
+
+        return result;
+    }
+
+    public string Pop()
+    {
+        string result = string.Empty;
+        if (!IsEmpty())
+        {
+            var lastIndex = data.Count - 1;
+            result = data[lastIndex];
+            data.RemoveAt(lastIndex);
+        }
+
+        return result;
+    }
+}
+
